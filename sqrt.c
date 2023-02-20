d434cf2ca39f47493403a03b651c6b24126c1d9d
TheAio <76646135+TheAio@users.noreply.github.com>
2023-02-20 10:56:30 +0100

    Create sqrt.c

---
+++ sqrt.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+   double number, squareRoot;
+   number = 32;
+   squareRoot = sqrt(number);
+   printf("%.2lf",squareRoot);
+   return 0;
+}
