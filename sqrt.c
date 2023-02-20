6e4b6551c7d5a7bf2258b8ea54b5e10de993c14b
The1temp1guy <125903172+The1temp1guy@users.noreply.github.com>
2023-02-20 08:12:33 -0800

    Without math.h
    Gives a more concrete understanding of c.

--- sqrt.c
+++ sqrt.c
@@ -1,10 +1,16 @@
-#include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
-   double number, squareRoot;
-   number = 32;
-   squareRoot = sqrt(number);
-   printf("%.2lf",squareRoot);
+   double input = 32;
+   double divtest = 2;
+   double fout;
+   int ten_k = 10000
+   int i;
+   for(i=0;i<ten_k;i++) {
+      fout = input/divtest;
+      divtest = (fout+divtest)/2
+   }
+   print("%lf\n", fout);
    return 0;
 }
