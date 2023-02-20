b77681da69e72b5671da303a21b3ce885eee2381
GPLuser5 <125906857+GPLuser5@users.noreply.github.com>
2023-02-20 17:06:04 +0000

    no math.h
    A sqrt algorithm implementation

--- sqrt.c
+++ sqrt.c
@@ -1,10 +1,17 @@
-#include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
-   double number, squareRoot;
-   number = 32;
-   squareRoot = sqrt(number);
-   printf("%.2lf",squareRoot);
+   double input;
+   input = 32;
+   double divtest = 2;
+   double fout;
+   int tenk = 10000;
+   int i;
+   for(i=0;i<=tenk;i++) {
+      fout = input/divtest;
+      divtest = (fout+divtest)/2;
+   }
+   printf("%lf\n",fout);
    return 0;
 }
